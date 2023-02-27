439b006117cd5fbbdcd7da7b04fb932fec80a830
hao <hao456c@gmail.com>
2023-02-27 17:24:23 +0700

    create basic database

--- database/migrations/2023_02_27_095139_create_relationship_table.php
+++ database/migrations/2023_02_27_095139_create_relationship_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('relationship', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password'); 
-            $table->boolean('receiveStrangerMessage')->default(true);
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('friend_id')->constrained('users');
+            $table->enum('status', ['friend', 'stranger', 'block'])->default('stranger');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('relationship');
     }
 };
--- database/migrations/2023_02_27_095539_create_friend_request_table.php
+++ database/migrations/2023_02_27_095539_create_friend_request_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('friend_request', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password'); 
-            $table->boolean('receiveStrangerMessage')->default(true);
-            $table->rememberToken();
+            $table->foreignId('sender')->constrained('users');
+            $table->foreignId('receiver')->constrained('users');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('friend_request');
     }
 };
